6fb99d79594e49ef6a75bcf0e579207e59077d55
bveretelnik <bveretelnik@gmail.com>
2025-04-11 11:43:38 +0200

    FT - fix styles and add currency calculate

--- src/components/NumberConverter/NumberConverter.jsx
+++ src/components/NumberConverter/NumberConverter.jsx
@@ -7,8 +7,9 @@ import replaceChar from '../../heplers/replaceChar.js';
 
 const NumberConverter = () => {
   const inputRef = useRef(null);
+  const inputCurRef = useRef(null);
   const [input, setInput] = useState('');
-  const [convertedNumber, setConvertedNumber] = useState('');
+  const [inputCur, setInputCur] = useState('');
 
   const convertNumberFormat = input => {
     return input.replace(/\s/g, '').replace(/\./g, ',');
@@ -17,35 +18,61 @@ const NumberConverter = () => {
   const handleInputChange = e => {
     const value = e.target.value;
     setInput(value);
-    setConvertedNumber(convertNumberFormat(value));
   };
 
-  const number = parseFloat(convertedNumber.replace(',', '.'));
+  const number =
+    parseFloat(input.replace(',', '.')) *
+    (parseFloat(inputCur.replace(',', '.')) || 1);
+  console.log(number);
   const epTax = replaceChar(number * EP);
   const militaryTax = replaceChar(number * VZ);
 
   return (
     <div className="convectorContainer">
+      <h1>Конвертер числа</h1>
       <div className="convectorWrapper">
-        <h1>Конвертер числа</h1>
-        <input
-          className="convectorInput"
-          type="text"
-          value={input}
-          ref={inputRef}
-          onChange={handleInputChange}
-          placeholder="Введіть число, наприклад: 3 288.50"
-        />
-        <ClearButton
-          onClick={() => {
-            setInput('');
-            inputRef.current.focus();
-          }}
-        />
-        <p>Натисніть на значення, щоб скопіювати його:</p>
-        <CopyToClipboard value={convertedNumber} label={'SUM:'} />
-        <CopyToClipboard value={epTax || 0} label={'ЄП:'} />
-        <CopyToClipboard value={militaryTax || 0} label={'ВЗ:'} />
+        <div className="convectorInputWrapper">
+          <div>
+            <input
+              className="convectorInput"
+              type="text"
+              value={input}
+              ref={inputRef}
+              onChange={handleInputChange}
+              placeholder="Введіть число, наприклад: 3 288.50"
+            />
+            <ClearButton
+              onClick={() => {
+                setInput('');
+                inputRef.current.focus();
+              }}
+            />
+          </div>
+          <div>
+            <input
+              className="convectorInput"
+              type="text"
+              value={inputCur}
+              ref={inputCurRef}
+              onChange={e => {
+                setInputCur(e.target.value);
+              }}
+              placeholder="Введіть суму у валюті"
+            />
+            <ClearButton
+              onClick={() => {
+                setInputCur('');
+                inputCurRef.current.focus();
+              }}
+            />
+          </div>
+        </div>
+        <div>
+          <p className="title">Натисніть на значення, щоб скопіювати його:</p>
+          <CopyToClipboard value={replaceChar(number)} label={'SUM:'} />
+          <CopyToClipboard value={epTax || 0} label={'ЄП:'} />
+          <CopyToClipboard value={militaryTax || 0} label={'ВЗ:'} />
+        </div>
       </div>
     </div>
   );
