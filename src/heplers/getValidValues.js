a4f958d61e6cd2dcfa2e531c11625061a137ef95
bveretelnik <bveretelnik@gmail.com>
2025-11-01 19:38:28 +0100

    FT - add fee constant

--- src/heplers/getValidValues.js
+++ src/heplers/getValidValues.js
@@ -1,6 +1,8 @@
 import replaceChar from './replaceChar.js';
 import { EP, VZ } from '../constants/taxConstants.js';
 
+const FEE = 2.2;
+
 const parseNumber = value =>
   parseFloat(value.replace(',', '.').replaceAll(' ', ''));
 
@@ -9,8 +11,8 @@ export const getValidValues = (value, valueVur) => {
 
   const exchangeRate = parseNumber(value) / (parseNumber(valueVur) || 1) || 0;
 
-  const epTax = replaceChar(number * EP);
-  const militaryTax = replaceChar(number * VZ);
+  const epTax = replaceChar(number * EP + FEE);
+  const militaryTax = replaceChar(number * VZ + FEE);
 
   return { number, epTax, militaryTax, exchangeRate };
 };
