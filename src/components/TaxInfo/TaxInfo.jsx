9cab019d412bc21919729206a41193b7d0d5e1f0
bveretelnik <bveretelnik@gmail.com>
2025-12-21 17:05:02 +0100

    FT - fix tax info component

--- src/components/TaxInfo/TaxInfo.jsx
+++ src/components/TaxInfo/TaxInfo.jsx
@@ -3,19 +3,18 @@ import styles from './TaxInfo.module.scss';
 import Card from '../Card/Card.jsx';
 import SubTitle from '../NumberConverter/SubTitle/SubTitle.jsx';
 import { taxValues } from '../../config.js';
+import CopyToClipboard from '../CopyToClipboard/CopyToClipboard.jsx';
 
 const TaxInfo = () => {
   const currentYear = new Date().getFullYear();
-  const taxes = taxValues[currentYear];
 
   return (
     <Card>
       <SubTitle value={`👾 Tax information for the ${currentYear} year`} />
       <ul className={styles.taxInfoList}>
-        {Object.entries(taxes).map(([key, value]) => (
+        {Object.entries(taxValues).map(([key, value]) => (
           <li key={key}>
-            {key.toUpperCase()} :{' '}
-            <strong className={styles.taxInfoValue}>{value}</strong> UAH
+            <CopyToClipboard value={value} label={`${key.toUpperCase()}:`} />
           </li>
         ))}
       </ul>
--- src/config.js
+++ src/config.js
@@ -3,14 +3,7 @@ export const NBU_API_BASE =
 export const DEFAULT_STALE_TIME = 1000 * 60 * 10;
 
 export const taxValues = {
-  2025: {
-    ep: '1600.00',
-    vz: '800.00',
-    esv: '1760.00',
-  },
-  2026: {
-    ep: '1729.4',
-    vz: '864.7',
-    esv: '1902.34',
-  },
+  ep: '1729.4',
+  vz: '864.7',
+  esv: '1902.34',
 };
